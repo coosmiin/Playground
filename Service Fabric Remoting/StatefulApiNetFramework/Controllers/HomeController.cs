20fa57c232a9b9182bcf16a1601d580320a9e85e
coosmiin <coosmiin@yahoo.com>
2019-03-14 22:24:02 +0200

    Added NetFramework stateful api as well

---
+++ Service Fabric Remoting/StatefulApiNetFramework/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using ActorNetFramework.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using System;
+
+namespace StatefulApiNetFramework.Controllers
+{
+	[Route("")]
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		public async void Index()
+		{
+			var actorNetFrameworkProxy = ActorProxy.Create<IActorNetFramework>(new ActorId(Guid.NewGuid()));
+
+			try
+			{
+				await actorNetFrameworkProxy.DoSomething(new FrameworkMessage { Content = "message", Index = 1 });
+			}
+			catch (Exception ex)
+			{
+			}
+		}
+	}
+}
