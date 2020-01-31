f047a6402ddb78adb9cc30b65d78bf3777b0a4b8
coosmiin <coosmiin@yahoo.com>
2020-01-31 20:40:11 +0200

    Added service fabric reliable service asp.net quick start

---
+++ Service Fabric - Enable ASP.NET Core Api Quick Start/StatelessApi/Controllers/DummyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace StatelessApi.Controllers
+{
+	[ApiController]
+	public class DummyController : ControllerBase
+	{
+		[HttpGet("dummy")]
+		public async Task<ActionResult> Get()
+		{
+			return Ok("ok");
+		}
+	}
+}
---
+++ Service Fabric - Enable ASP.NET Core Api Quick Start/StatelessApi/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StatelessApi
+{
+	internal class Startup
+	{
+		public IConfiguration Configuration { get; }
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvcCore();
+		}
+
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			app.UseMvc();
+		}
+	}
+}
